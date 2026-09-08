b87c57a337b79dd4a4ca28871d4ef04a6cde3e7c
Ольга Денисова <olgadenisova@MacBook-Air-Fox.local>
2026-09-09 01:14:39 +0300

    Add pattern examples to Main

--- src/main/java/ru/oliya/Main.java
+++ src/main/java/ru/oliya/Main.java
@@ -3,29 +3,74 @@ package ru.oliya;
 import builder.Product;
 import chain.*;
 import strategy.*;
+import decorator.EveningDelivery;
+import decorator.ExpressDelivery;
+import adapter.ExternalMedicalApi;
+import adapter.PrescriptionAdapter;
+import adapter.PrescriptionPrinter;
+import adapter.PrescriptionService;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        // Пример использования паттерна Strategy:
+        // Заказ будет доставлен курьером
+
         DeliveryStrategy deliveryStrategy = new CourierDelivery();
         OrderService orderService = new OrderService(deliveryStrategy);
         orderService.deliverOrder();
+
+        // Заказ помещен в постамат
 
         DeliveryStrategy deliveryStrategy1 = new PostamatDelivery();
         OrderService orderService1 = new OrderService(deliveryStrategy1);
         orderService1.deliverOrder();
 
+        // Заказ подготовлен для самовывоза
+
         DeliveryStrategy deliveryStrategy2 = new PickupDelivery();
         OrderService orderService2 = new OrderService(deliveryStrategy2);
         orderService2.deliverOrder();
+
+        // Заказ передан в СДЭК
 
         DeliveryStrategy deliveryStrategy3 = new CdekDelivery();
         OrderService orderService3 = new OrderService(deliveryStrategy3);
         orderService3.deliverOrder();
 
         System.out.println();
+
+        // Пример использования паттерна Декоратор:
+        // Обертка курьер + экспресс доставка
+
+        DeliveryStrategy delivery = new ExpressDelivery(new CourierDelivery());
+        orderService = new OrderService(delivery);
+        orderService.deliverOrder();
+
+        System.out.println();
+
+        // Обертка курьер + вечерняя доставка
+
+        DeliveryStrategy delivery1 = new EveningDelivery(new CourierDelivery());
+        orderService1 = new OrderService(delivery1);
+        orderService1.deliverOrder();
+
+        System.out.println();
+
+        // Обертка курьер + экспресс доставка + вечерняя доставка
+
+        DeliveryStrategy delivery2 =
+                new EveningDelivery(new ExpressDelivery(new CourierDelivery()));
+        orderService2 = new OrderService(delivery2);
+        orderService2.deliverOrder();
+
+        System.out.println();
+
+        // Пример использования паттерна Chain of Responsibility:
+        // на примере цепочки обработки заказа
+        // (авторизация -> наличие товара -> наличие рецепта -> адрес)
 
         AuthorizationCheck authorizationCheck = new AuthorizationCheck();
         AvailabilityCheck availabilityCheck = new AvailabilityCheck();
@@ -42,6 +87,9 @@ public class Main {
         authorizationCheck.check(order);
 
         System.out.println();
+
+        // Пример использования паттерна Builder:
+        // на примере сборки 2-х продуктовых карточек
 
         Product product = new Product.Builder()
                 .name("Парацетамол")
@@ -68,6 +116,18 @@ public class Main {
                 .deliveryAvailable(true)
                 .build();
         product1.printProduct();
+
+        System.out.println();
+
+        // Пример использования паттерна Адаптер
+
+        PrescriptionService prescriptionService =
+                new PrescriptionAdapter(new ExternalMedicalApi());
+
+        PrescriptionPrinter.printResult(prescriptionService, "RX-458_1");
+        PrescriptionPrinter.printResult(prescriptionService, "RX-123");
+
+        System.out.println();
     }
 
 }
